44fb5fa1351b0c55598d231b984fb305e2ebf3e3
Frondor <frondor.dev@gmail.com>
2019-09-21 18:07:07 -0300

    Upgrade dependencies

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const pkg = require("../package.json");
-const common = require("./webpack.common");
+const path = require('path');
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const pkg = require('../package.json');
+const common = require('./webpack.common');
 
 const optionalCSS = [];
 
-if (pkg.devDependencies["css-loader"]) {
-  const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-  const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-  const sass = require("./webpack.sass");
+if (pkg.devDependencies['css-loader']) {
+  const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+  const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+  const sass = require('./webpack.sass');
 
   const optimizeCSS = {
     optimization: {
@@ -17,29 +17,27 @@ if (pkg.devDependencies["css-loader"]) {
         new UglifyJsPlugin({
           cache: true,
           parallel: true,
-          sourceMap: true // set to true if you want JS source maps
+          sourceMap: true, // set to true if you want JS source maps
         }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
-    }
+        new OptimizeCSSAssetsPlugin({}),
+      ],
+    },
   };
 
   optionalCSS.push(sass, optimizeCSS);
 }
 
 const prod = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   plugins: [
-    new CleanWebpackPlugin(["dist"], {
-      root: path.resolve(__dirname, "..")
-    })
-  ]
+    new CleanWebpackPlugin(),
+  ],
 };
 
 const WebConfig = merge.smart(common, prod, ...optionalCSS);
 const NodeConfig = merge.smart(WebConfig, {
-  target: "node"
+  target: 'node',
 });
 
 const targets = [];
@@ -51,8 +49,8 @@ if (pkg.browser) {
   if (/\.node\.js$/.test(pkg.main)) {
     // avoid name conflicts
     NodeConfig.output.filename = NodeConfig.output.filename.replace(
-      ".js",
-      ".node.js"
+      '.js',
+      '.node.js'
     );
     targets.push(NodeConfig);
   }
